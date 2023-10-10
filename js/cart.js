414f0c4e08db919276b914671723a05a8df1b916
lucasmiraballes <lucas20miraballes@gmail.com>
2023-10-10 00:53:31 -0300

    funciones carrito

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,71 @@
+function calcularTotal() {
+  let productos = document.querySelectorAll("#productos tr");
+  let total = 0;
+  
+  productos.forEach((producto) => {
+    let cantidad = parseInt(producto.querySelector("#cantidad").value);
+    let precioUnitario = parseFloat(producto.querySelector("#precio").textContent.replace(/[^0-9.-]+/g,"")); // Eliminar caracteres no numéricos
+    let subtotal = cantidad * precioUnitario;
+    
+    producto.querySelector("#subtotal").textContent = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(subtotal);
+    
+    total += subtotal;
+  });
+
+  let totalCompra = document.getElementById("totalCompra")
+  totalCompra.textContent = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(total);
+
+  let formaPago = document.querySelector('input[name="formaPago"]:checked').value;
+  let mensajeFormaPago = '';
+
+  let precioFinal = total; // Inicializar precioFinal con el total sin descuento o aumento
+
+  switch (formaPago) {
+    case 'efectivo':
+      mensajeFormaPago = 'Efectivo (10% de descuento)';
+      precioFinal = total * 0.9; // aplica descuento del 10% y lo mestra
+      break;
+    case 'credito':
+      mensajeFormaPago = 'Crédito (7% de aumento)';
+      precioFinal = total * 1.07; // aplica aumento del 7% y lo muestra
+      break;
+    case 'debito':
+      mensajeFormaPago = 'Débito';
+      break;
+  }
+
+  document.getElementById("formaPagoSeleccionada").textContent = mensajeFormaPago;
+  
+  // Mostrar el precio final
+  document.getElementById("precioFinal").textContent = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(precioFinal);
+
+  let mensajeFinal = document.getElementById("mensajeFinal")
+    let comprar = document.getElementById("comprar")
+    comprar.addEventListener("click", () => {
+      if (productos.length > 0){
+        mensajeFinal.innerHTML = `<br><br>
+        <h3>Compra realizada correctamente</h3>
+        <p>Precio final: $${precioFinal}</p>
+        <p>Forma de pago: ${formaPago}</p>`
+    } else {
+        mensajeFinal.innerHTML = `<br><br>
+        <h3>No hay productos en su carrito</h3>`
+        mensajeFinal.classList.add("text-danger")
+    } 
+  })
+}
+
 function cargarCarrito() {
   let productos = JSON.parse(localStorage.getItem("cart"));
   productos.forEach((producto) => {
     document.getElementById("productos").innerHTML += `<tr id="prod">
             <td>${producto.nombre}</td>
             <td id="precio">
-              ${Intl.NumberFormat("en-US").format(
-                producto.costo_en_pesos.toFixed(2)
-              )}
+              ${Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(producto.costo_en_pesos)}
             </td>
             <td><input id="cantidad" class="form-control" type="number" value="1"></input></td>
             <td id="subtotal">
-              ${Intl.NumberFormat("en-US").format(
-                producto.costo_en_pesos.toFixed(2)
-              )}
+              ${Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(producto.costo_en_pesos)}
             </td>
             <td>
               <button id="del" class="btn btn-danger">
@@ -22,6 +75,7 @@ function cargarCarrito() {
             <td id="id" class="d-none" hidden>${producto.identificacion}</td> 
           </tr>`;
   });
+
   document.querySelectorAll("#prod").forEach((prod) => {
     prod.querySelector("#cantidad").addEventListener("change", (e) => {
       let cantidad = e.target.value;
@@ -31,10 +85,11 @@ function cargarCarrito() {
       }
       let subtotal = prod.querySelector("#subtotal");
       let precio = prod.querySelector("#precio");
-      subtotal.textContent = Intl.NumberFormat("en-US").format(
-        (precio.textContent * cantidad).toFixed(2)
-      );
+      subtotal.textContent = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format((precio.textContent.replace(/[^0-9.-]+/g,"") * cantidad).toFixed(2));
+      
+      calcularTotal(); // Llamamos a la función calcularTotal() cuando cambia la cantidad
     });
+
     prod.querySelector("#del").addEventListener("click", () => {
       prod.remove();
       let productos = JSON.parse(localStorage.getItem("cart"));
@@ -42,7 +97,18 @@ function cargarCarrito() {
         return producto.identificacion != prod.querySelector("#id").textContent;
       });
       localStorage.setItem("cart", JSON.stringify(productos));
+      
+      calcularTotal(); // Llamamos a la función calcularTotal() cuando se elimina un producto
     });
   });
 }
-document.addEventListener("DOMContentLoaded", cargarCarrito());
+
+document.addEventListener("DOMContentLoaded", () => {
+  cargarCarrito();
+  calcularTotal();
+});
+
+// Llamar a calcularTotal() cuando se cambie la forma de pago
+document.querySelectorAll('input[name="formaPago"]').forEach((input) => {
+  input.addEventListener("change", calcularTotal);
+});
--- js/index.js
+++ js/index.js
@@ -9,28 +9,69 @@ async function fetchProducts() {
   } catch (error) {
     console.log(error);
   }
-  console.log(productos);
   return productos;
 }
+
+const searchInput = document.getElementById("searchInput");
+
 function showProducts() {
   fetchProducts().then((productos) => {
-    productos.productos.forEach((producto) => {
-      document.getElementById(
-        "productos"
-      ).innerHTML += `<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 mb-3">
-          <div class="card shadow">
-            <div class="card-body">
-              <h5 class="card-title">${producto.nombre}</h5>
-              <p class="card-text">
-                ${producto.descripcion}
-              </p>
-              <button class="btn btn-info" onclick="addToCart('${producto.identificacion}')"> Agregar al carrito </button>
-            </div>
-          </div>
-        </div>`;
+    // cuando el usuario busca en el filtrado de busqueda
+    searchInput.addEventListener("input", () => {
+      const searchTerm = searchInput.value.toLowerCase();
+
+      const filteredProducts = productos.productos.filter((producto) => {
+        // filtra los productos en nombre y descripcion segun lo que busca el usuario
+        return (
+          producto.nombre.toLowerCase().includes(searchTerm) ||
+          producto.descripcion.toLowerCase().includes(searchTerm)
+        );
+      });
+
+      // muestra los productos filter
+      renderProducts(filteredProducts);
     });
+
+    // muestra todos los productos al cargar la pagina, funcion que esta abajo reemplazada 
+    renderProducts(productos.productos);
   });
 }
+
+function renderProducts(productList) {
+  const productosContainer = document.getElementById("productos");
+  productosContainer.innerHTML = "";
+
+  productList.forEach((producto) => {
+    const card = document.createElement("div");
+    card.classList.add("col-sm-12", "col-md-6", "col-lg-4", "col-xl-4", "mb-3");
+    card.innerHTML = `
+      <div class="card shadow border-card">
+        <div class="card-body">
+          <h5 class="card-title text-card-margin">${producto.nombre}</h5>
+          <p class="card-text card-description">
+            ${producto.descripcion}
+          </p>
+          <p class="card-text">Stock: <span class="stock-q">${producto.cantidad_en_stock}</span></p>
+          <button class="btn btn-info" onclick="addToCart('${producto.identificacion}')">Agregar al carrito</button>
+        </div>
+      </div>
+    `;
+
+    card.querySelector(".btn-info").addEventListener("click", () => {
+      const stockElement = card.querySelector(".stock-q");
+      const currentStock = parseInt(stockElement.textContent);
+      if (currentStock > 0) {
+        stockElement.textContent = currentStock - 1;
+
+        // Actualiza el stock en el objeto del producto
+        producto.cantidad_en_stock = currentStock - 1;
+      }
+    });
+
+    productosContainer.appendChild(card);
+  });
+}
+
 
 function addToCart(id) {
   let cart = [];
@@ -46,8 +87,7 @@ function addToCart(id) {
     localStorage.setItem("cart", JSON.stringify(cart));
 
     const toastLiveExample = document.getElementById("liveToast");
-    const toastBootstrap =
-      bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    const toastBootstrap = new bootstrap.Toast(toastLiveExample);
     toastBootstrap.show();
   });
 }
